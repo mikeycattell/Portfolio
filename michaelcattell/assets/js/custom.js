e65cdd9ff031842acd67060782b663cd6b251de5
mikeycattell <michael.a.cattell@gmail.com>
2013-08-14 11:48:56 -0700

    Initial upload
    No new changes - just an upload

---
+++ michaelcattell/assets/js/custom.js
@@ -0,0 +1,6 @@
+var buy_animation=setInterval(function()
+{$("#purchaseLink a").removeClass().addClass("swing animated"),window.setTimeout(function()
+	{$("#purchaseLink a").removeClass()},1300)},5e3);
+
+
+
