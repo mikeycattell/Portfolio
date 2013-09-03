a1fe4d4cc41632b9c42fa5b214f907531910a17f
mikeycattell <michael.a.cattell@gmail.com>
2013-09-03 16:55:45 -0700

    minor improvements
    Line height on nav, unbold nav

---
+++ michaelcattell/converted-html/assets/js/custom.js
@@ -0,0 +1,37 @@
+var buy_animation=setInterval(function()
+{$("#purchaseLink a").removeClass().addClass("swing animated"),window.setTimeout(function()
+	{$("#purchaseLink a").removeClass()},1300)},5e3);
+
+// Returns true if the specified element has been scrolled into the viewport.
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.fadeInImg');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
